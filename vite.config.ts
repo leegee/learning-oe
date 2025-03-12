2bb750fad792926b957ab122ca59b3b4d2dee7c9
Lee Goddard <leegee@gmail.com>
2025-03-12 14:09:27 +0100

    Extrapolate gh URL from packagejson for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import packageJson from './package.json';
+
+const homepage = packageJson.homepage || '/';
+const base = homepage ? (new URL(homepage)).pathname.replace(/\/?$/, '/') : '/';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base,
   plugins: [
     react(),
     VitePWA({
