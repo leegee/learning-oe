52a07c4b2a4060918a6a475d8267245a38b6058e
Lee Goddard <leegee@gmail.com>
2025-03-12 14:14:32 +0100

    log

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import packageJson from './package.json';
 const homepage = packageJson.homepage || '/';
 const base = homepage ? (new URL(homepage)).pathname.replace(/\/?$/, '/') : '/';
 
+console.info(['='.repeat(30), 'Base: ' + base, "=".repeat(30)].join("\n"))
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base,
