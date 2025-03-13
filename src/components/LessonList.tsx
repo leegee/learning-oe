8e9b112dc4f323ba3dab42a8db19c6703146144e
Lee Goddard <leegee@gmail.com>
2025-03-13 15:45:13 +0100

    No longer confirm choices...ho hum

--- src/components/LessonList.tsx
+++ src/components/LessonList.tsx
@@ -2,7 +2,6 @@
 
 import { useTranslation } from "react-i18next";
 import { LessonSummary } from "../Lessons";
-import ConfirmDialog from "./dialogs/Confirm";
 import './LessonList.css';
 import { useState } from "react";
 
@@ -14,57 +13,33 @@ interface LessonListProps {
 
 const LessonList = ({ lessons, currentLessonIndex, onLessonSelected }: LessonListProps) => {
     const { t } = useTranslation();
-    const [isDialogOpen, setIsDialogOpen] = useState(false);
-    const [lessonIndexToChange, setLessonIndexToChange] = useState<number | null>(null);
 
     const onLessonSelectedLocal = (lessonIndex: number) => {
-        setLessonIndexToChange(lessonIndex);
-        setIsDialogOpen(true);
-    };
-
-    const handleConfirmChangeLesson = () => {
-        setIsDialogOpen(false);
-        if (lessonIndexToChange !== null) {
-            onLessonSelected(lessonIndexToChange);
-        }
-    };
-
-    const handleCancelChangeLesson = () => {
-        setLessonIndexToChange(null);
-        setIsDialogOpen(false);
+        onLessonSelected(lessonIndex);
     };
 
     return (
-        <>
-            <section className="card lesson-list">
-                <h2>{t('list_lessons_title')}</h2>
-                <ol >
-                    {lessons.map((lessonSummary, index) => (
-                        <li key={index}>
-                            <button
-                                {...(index > currentLessonIndex && { disabled: true })}
-                                onClick={() => { if (index <= currentLessonIndex) { onLessonSelectedLocal(index) } }}
-                                className={[
-                                    index < currentLessonIndex && 'completed',
-                                    index === currentLessonIndex && 'current',
-                                    index > currentLessonIndex && 'todo'
-                                ].filter(Boolean).join(' ')}
-                            >
-                                <span className='index'>{index + 1}</span>
-                                {lessonSummary.title}
-                            </button>
-                        </li>
-                    ))}
-                </ol>
-            </section >
-
-            <ConfirmDialog
-                isOpen={isDialogOpen}
-                message={t('will_you_learn_lesson_n', { n: lessonIndexToChange !== null ? lessonIndexToChange + 1 : 1 })}
-                onConfirm={handleConfirmChangeLesson}
-                onCancel={handleCancelChangeLesson}
-            />
-        </>
+        <section className="card lesson-list">
+            <h2>{t('list_lessons_title')}</h2>
+            <ol >
+                {lessons.map((lessonSummary, index) => (
+                    <li key={index}>
+                        <button
+                            {...(index > currentLessonIndex && { disabled: true })}
+                            onClick={() => { if (index <= currentLessonIndex) { onLessonSelectedLocal(index) } }}
+                            className={[
+                                index < currentLessonIndex && 'completed',
+                                index === currentLessonIndex && 'current',
+                                index > currentLessonIndex && 'todo'
+                            ].filter(Boolean).join(' ')}
+                        >
+                            <span className='index'>{index + 1}</span>
+                            {lessonSummary.title}
+                        </button>
+                    </li>
+                ))}
+            </ol>
+        </section >
     );
 };
 
