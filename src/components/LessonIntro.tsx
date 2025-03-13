1701ec02b23198dfccffd4559d1d0aaf7d5acc9c
Lee Goddard <leegee@gmail.com>
2025-03-13 09:13:05 +0100

    Extrapolate About

--- src/components/LessonIntro.tsx
+++ src/components/LessonIntro.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import { useTranslation } from "react-i18next";
 import './LessonIntro.css';
+import AboutComponent from "./About";
 
 interface LessonIntroProps {
     title: string;
@@ -13,39 +14,26 @@ const LessonIntro = ({ title, index, children, onContinue }: LessonIntroProps) =
     const { t } = useTranslation();
 
     return (
-        <>
-            <article className="lesson-intro">
-                <section className="card">
-                    <h2>
-                        {t('lesson')} {index + 1}
-                    </h2>
-                    <h3>{title}</h3>
-                    <div className="buttons">
-                        <button className='next-button' onClick={onContinue}>{t('begin')}</button>
-                    </div>
-                </section>
+        <article className="lesson-intro">
+            <section className="card">
+                <h2>
+                    {t('lesson')} {index + 1}
+                </h2>
+                <h3>{title}</h3>
+                <div className="buttons">
+                    <button className='next-button' onClick={onContinue}>{t('begin')}</button>
+                </div>
+            </section>
 
-                {children && (
-                    <>
-                        {children}
-                    </>
-                )}
+            {children && (
+                <>
+                    {children}
+                </>
+            )}
 
-                <section className="card about">
-                    <h2>About</h2>
-                    <p>
-                        This little app was written in under a week to help me
-                        learn a little Old English, but written in such a way
-                        that it can easily be reconfigured with just a text editor
-                        to quiz the user on any subject.
-                    </p>
-                    <footer>
-                        &mdash; <a title='E-mail' href='mailto:leegee@gmail.com'>Lee Goddard</a>, Gödöllő, 2025
-                    </footer>
-                </section>
-            </article >
+            <AboutComponent />
 
-        </>
+        </article >
     );
 };
 
