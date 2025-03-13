456b0956f66158097dea1fdd0e5e2308dec8847b
Lee Goddard <leegee@gmail.com>
2025-03-13 09:06:42 +0100

    About info

--- src/components/LessonIntro.tsx
+++ src/components/LessonIntro.tsx
@@ -31,7 +31,18 @@ const LessonIntro = ({ title, index, children, onContinue }: LessonIntroProps) =
                     </>
                 )}
 
-
+                <section className="card about">
+                    <h2>About</h2>
+                    <p>
+                        This little app was written in under a week to help me
+                        learn a little Old English, but written in such a way
+                        that it can easily be reconfigured with just a text editor
+                        to quiz the user on any subject.
+                    </p>
+                    <footer>
+                        &mdash; <a title='E-mail' href='mailto:leegee@gmail.com'>Lee Goddard</a>, Gödöllő, 2025
+                    </footer>
+                </section>
             </article >
 
         </>
