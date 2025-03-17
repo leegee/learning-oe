b6b41f3586656ff4b2bfa9df340d3c7cceddb585
Lee Goddard <leegee@gmail.com>
2025-03-17 12:32:14 +0100

    Skip button

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -1,18 +1,28 @@
 import { useTranslation } from "react-i18next";
-
+import { useEffect, useState } from "react";
 import './ActionButton.css';
 
 interface ButtonProps {
     isCorrect: boolean | null;
     isInputPresent: boolean;
+    minClickBeforeSkip?: number;
     onCheckAnswer: () => void;
     onComplete: () => void;
 }
 
-const ActionButton = ({ isCorrect, isInputPresent, onCheckAnswer, onComplete, }: ButtonProps) => {
+const ActionButton = ({ isCorrect, isInputPresent, minClickBeforeSkip, onCheckAnswer, onComplete }: ButtonProps) => {
     const { t } = useTranslation();
+    const [clicksCounted, setClicksCounted] = useState<number>(0);
+
+    minClickBeforeSkip = minClickBeforeSkip || 2;
+
+    useEffect(() => {
+        setClicksCounted(0);
+    }, [isCorrect, isInputPresent]);
 
     const handleClick = () => {
+        setClicksCounted((prev) => prev + 1);
+
         if (isCorrect === true) {
             onComplete();
         } else {
@@ -21,23 +31,29 @@ const ActionButton = ({ isCorrect, isInputPresent, onCheckAnswer, onComplete, }:
     };
 
     return (
-        <button
-            className={
-                isCorrect === null
-                    ? 'next-button'
-                    : isCorrect === false
-                        ? 'try-again-button'
-                        : 'next-button'
-            }
-            onClick={handleClick}
-            disabled={!isInputPresent}
-        >
-            {isCorrect === null
-                ? t('next')
-                : isCorrect === true
-                    ? t('correct_next')
-                    : t('try_again')}
-        </button>
+        <>
+            <button
+                className={
+                    isCorrect === null
+                        ? 'next-button'
+                        : isCorrect === false
+                            ? 'try-again-button'
+                            : 'next-button'
+                }
+                onClick={handleClick}
+                disabled={!isInputPresent}
+            >
+                {isCorrect === null
+                    ? t('next')
+                    : isCorrect === true
+                        ? t('correct_next')
+                        : t('try_again')}
+            </button>
+
+            {clicksCounted >= minClickBeforeSkip && (
+                <button className='skip-button' onClick={onComplete}>{t('skip')}</button>
+            )}
+        </>
     );
 };
 
