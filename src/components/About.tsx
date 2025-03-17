489ab29968a06fb9b0c827bfa572abc5d12bd8f2
Lee Goddard <leegee@gmail.com>
2025-03-17 12:34:33 +0100

    txt

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,9 +7,11 @@ const AboutComponent = () => {
             <h2>About This App</h2>
             <p>
                 This little app was written in under a week to help me
-                learn a little Old English, but so that it can easily be
-                reconfigured with just a text editor to quiz the user on
-                any subject. Nothing is stored outside of your device.
+                learn a little Old English, but it can easily be altered
+                with just a text editor to quiz the user on any subject.
+            </p>
+            <p>
+                Nothing is stored outside of your device.
             </p>
             <footer>
                 &mdash; <a title='E-mail' href='mailto:leegee@gmail.com'>Lee Goddard</a>, Gödöllő, 2025
